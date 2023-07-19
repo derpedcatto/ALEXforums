a20569657e0588a68c34ae75b17c109bdf2a569b
Alexander Vannovskiy <sashavannovski@gmail.com>
2023-07-19 19:36:02 +0300

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ALEXforums.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ALEXforums.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
