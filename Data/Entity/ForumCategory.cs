a12574e4edb21d78117d234fc677a5875508205a
Alexander Vannovskiy <sashavannovski@gmail.com>
2023-08-28 14:17:01 +0300

    Adding 'FaIcon' to Category table

--- Data/Entity/ForumCategory.cs
+++ Data/Entity/ForumCategory.cs
@@ -4,6 +4,7 @@
     {
         public Guid Id { get; set; }
         public string Name { get; set; } = null!;
+        public string FaIcon { get; set; } = null!;
 
         // Nav property
         public ICollection<ForumPost> ForumPosts { get; set; } = new List<ForumPost>();
---
+++ Migrations/20230817222909_ALEXforums_Update-1.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ALEXforums.Migrations
+{
+    /// <inheritdoc />
+    public partial class ALEXforums_Update1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            /*
+            migrationBuilder.DropForeignKey(
+                name: "FK_ForumPostComments_ForumPosts_PostId",
+                schema: "ALEXforums",
+                table: "ForumPostComments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ForumPostComments_Users_UserId",
+                schema: "ALEXforums",
+                table: "ForumPostComments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ForumPosts_ForumCategories_CategoryId",
+                schema: "ALEXforums",
+                table: "ForumPosts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ForumPosts_Users_UserId",
+                schema: "ALEXforums",
+                table: "ForumPosts");
+            */
+
+            migrationBuilder.AddColumn<string>(
+                name: "FaIcon",
+                schema: "ALEXforums",
+                table: "ForumCategories",
+                type: "longtext",
+                nullable: false);
+
+            /*
+            migrationBuilder.AddForeignKey(
+                name: "FK_ForumPostComments_ForumPosts_PostId",
+                schema: "ALEXforums",
+                table: "ForumPostComments",
+                column: "PostId",
+                principalSchema: "ALEXforums",
+                principalTable: "ForumPosts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ForumPostComments_Users_UserId",
+                schema: "ALEXforums",
+                table: "ForumPostComments",
+                column: "UserId",
+                principalSchema: "ALEXforums",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ForumPosts_ForumCategories_CategoryId",
+                schema: "ALEXforums",
+                table: "ForumPosts",
+                column: "CategoryId",
+                principalSchema: "ALEXforums",
+                principalTable: "ForumCategories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ForumPosts_Users_UserId",
+                schema: "ALEXforums",
+                table: "ForumPosts",
+                column: "UserId",
+                principalSchema: "ALEXforums",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+            */
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            /*
+            migrationBuilder.DropForeignKey(
+                name: "FK_ForumPostComments_ForumPosts_PostId",
+                schema: "ALEXforums",
+                table: "ForumPostComments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ForumPostComments_Users_UserId",
+                schema: "ALEXforums",
+                table: "ForumPostComments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ForumPosts_ForumCategories_CategoryId",
+                schema: "ALEXforums",
+                table: "ForumPosts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ForumPosts_Users_UserId",
+                schema: "ALEXforums",
+                table: "ForumPosts");
+            */
+
+            migrationBuilder.DropColumn(
+                name: "FaIcon",
+                schema: "ALEXforums",
+                table: "ForumCategories");
+
+            /*
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ForumPostComments_ForumPosts_PostId",
+                schema: "ALEXforums",
+                table: "ForumPostComments",
+                column: "PostId",
+                principalSchema: "ALEXforums",
+                principalTable: "ForumPosts",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ForumPostComments_Users_UserId",
+                schema: "ALEXforums",
+                table: "ForumPostComments",
+                column: "UserId",
+                principalSchema: "ALEXforums",
+                principalTable: "Users",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ForumPosts_ForumCategories_CategoryId",
+                schema: "ALEXforums",
+                table: "ForumPosts",
+                column: "CategoryId",
+                principalSchema: "ALEXforums",
+                principalTable: "ForumCategories",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ForumPosts_Users_UserId",
+                schema: "ALEXforums",
+                table: "ForumPosts",
+                column: "UserId",
+                principalSchema: "ALEXforums",
+                principalTable: "Users",
+                principalColumn: "Id");
+            */
+        }
+    }
+}
